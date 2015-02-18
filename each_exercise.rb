dc8e65b3986b55d3633d172bfa5b178fca4edeb7
Andy <andrew.sunglae.kim@gmail.com>
2015-02-18 16:41:41 -0500

    minor change

--- each_exercise.rb
+++ each_exercise.rb
@@ -1,5 +1,14 @@
-array = [1,2,3,4,5,6,7,8,9]
+numbers_in_order = [1,2,3,4,5,6,7,8,9]
+numbers_out_of_order = [2,6,4,3,5,7,1,8,9]
 sum = 0
+
+i = 0
+
+while i < numbers_in_order.length
+	puts i
+	i += 1
+end
+
 # use a ruby loop to find the sum of all the integers within the array
 
 # use the each method to find the sum
